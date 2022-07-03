141b84b6f776ee5d92e2f029e726f0063f225a22
Pavel <pavel.chernenko97@gmail.com>
2022-07-03 20:36:00 +0300

    add comment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// counter shop
 const counter = document.querySelector('.menu__counter-number');
 const product = document.querySelectorAll('.shop__product-list-link');
 var count = 0;
@@ -7,6 +8,5 @@ function onProductClick(item) {
     item.addEventListener("click", function() {
         count = count + 1;
         counter.innerHTML = count;
-        console.log(counter);
     })
 }
