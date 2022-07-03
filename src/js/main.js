c185cb4a6a5c8547bdfd8355b77a274fda5139cf
Pavel <pavel.chernenko97@gmail.com>
2022-07-03 20:34:04 +0300

    add working counter

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+const counter = document.querySelector('.menu__counter-number');
+const product = document.querySelectorAll('.shop__product-list-link');
+var count = 0;
+product.forEach(onProductClick);
+
+function onProductClick(item) {
+    item.addEventListener("click", function() {
+        count = count + 1;
+        counter.innerHTML = count;
+        console.log(counter);
+    })
+}
